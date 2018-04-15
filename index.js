a4a340218f4dc789a94ab74d3cb350656b80c4fb
Michael Bloch <michaelbloch1991@gmail.com>
2018-04-15 14:25:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout() {
+  console.log("HELLO")
+}
+
+function logWhisper() {
+  console.log("hello")
+}
+
+function sayHiToGrandma(string) {
+  if string == {
+    console.log("I can't hear you!")
+  } else {
+    console.log("YES INDEED!")
+  }
+  
+  
+}
