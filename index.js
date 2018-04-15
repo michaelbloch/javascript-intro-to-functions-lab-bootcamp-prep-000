f1a998f591a0ec6f4acae6bb6cd66ed04df8a198
Michael Bloch <michaelbloch1991@gmail.com>
2018-04-15 14:36:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function logWhisper() {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    console.log("YES INDEED!")
-  } else if (string.toLowerCase() === string) {
     console.log("I can't hear you!")
+  } else if (string.toLowerCase() === string) {
+    console.log("YES INDEED!")
   }
 }
